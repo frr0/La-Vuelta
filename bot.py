5fe957a416e352ca4598adba9120648fb30234b3
fraR-coder <francesco.mail5.0@gmail.com>
2023-02-24 11:59:51 +0100

    ora funziona tutto no abc e PT1,PT2

--- bot.py
+++ bot.py
@@ -1,4 +1,3 @@
-from cgi import print_arguments
 from telegram.ext.updater import Updater
 from telegram.update import Update
 from telegram.ext.callbackcontext import CallbackContext
@@ -7,18 +6,10 @@ from telegram.ext.messagehandler import MessageHandler
 from telegram.ext.filters import Filters
 from telegram import KeyboardButton, ReplyKeyboardMarkup
 from collections import OrderedDict
-import requests
-import sys
 
-import json
-import f
-# Content of f.py
-# # Module: f.py
-# from telegram.ext.updater import Updater
-# updater = Updater("Token", use_context=True)
 
 import requests
-import csv
+
 from bs4 import BeautifulSoup
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver import Chrome
@@ -27,12 +18,15 @@ from selenium.webdriver.common.by import By
 
 from testScript import *
 
+TOKEN=""
+updater = Updater(TOKEN, use_context=True)
+
 t_1 = "/team1"
 t_2 = "/team2"
 t_3 = "/ranking"
 
-PT1 = 0
-PT2 = 0
+PT1 = "0"
+PT2 = "0"
 
 def start(update: Update, context: CallbackContext):
     buttons = [[KeyboardButton(t_1)], [KeyboardButton(t_2)], [KeyboardButton(t_3)]]
@@ -76,16 +70,17 @@ stage=writeDataInDictionary()
 
 #a cosa serve sta roba sotto?
 
-#abc = ""
+abc = ""
+ran=""
 #n = 10
 #m = 0
- 
+#
 #for line in lines:
 #    line2 = line.text.replace("\n", " ").replace("\n\n\n\n", "").replace("      ", ",").replace("   ", ",").replace("  ", ",").replace(",,,,,,,,,,,,,", ",").replace(",,,,,,,,,,,,,,,,", ",").replace(",,,,",",").replace(",,,",",").replace(",,",",")
 #    if m < n:
 #        abc = abc + "\n" + line2
 #        m = m + 1
-#
+
 
 for nn in stage:
     if nn < 21:
@@ -93,8 +88,7 @@ for nn in stage:
         nn = nn + 1
     else:
         break
-print("\n\n\n\n\n\n")
-print(ran)
+
 
 # here are the dictionary of the teams
 
@@ -122,22 +116,18 @@ T2 = T2 + "\n\nPoints: " + str(pt2)
 with open('Points.txt', 'a', newline='',encoding="utf-8") as f:
     f.write(str(pt1)+'\n'+str(pt2)+'\n')
             
-# print()
-# print()
-# print("team1 points:")
-# print(pt1)
-# print("team1 points:")
-# print(pt2)
 
-f.updater.dispatcher.add_handler(CommandHandler('start', start))
-f.updater.dispatcher.add_handler(CommandHandler('team1', team_1))
-f.updater.dispatcher.add_handler(CommandHandler('team2', team_2))
-f.updater.dispatcher.add_handler(CommandHandler('ranking', ranking))
-f.updater.dispatcher.add_handler(CommandHandler('help', help))
-f.updater.dispatcher.add_handler(MessageHandler(Filters.text, unknown))
-f.updater.dispatcher.add_handler(MessageHandler(Filters.command, unknown))
-f.updater.dispatcher.add_handler(MessageHandler(Filters.text, unknown_text))
-f.updater.start_polling()
+
+updater.dispatcher.add_handler(CommandHandler('start', start))
+updater.dispatcher.add_handler(CommandHandler('team1', team_1))
+updater.dispatcher.add_handler(CommandHandler('team2', team_2))
+updater.dispatcher.add_handler(CommandHandler('ranking', ranking))
+updater.dispatcher.add_handler(CommandHandler('help', help))
+updater.dispatcher.add_handler(MessageHandler(Filters.text, unknown))
+updater.dispatcher.add_handler(MessageHandler(Filters.command, unknown))
+updater.dispatcher.add_handler(MessageHandler(Filters.text, unknown_text))
+updater.start_polling()
+updater.idle()
 
 
         
